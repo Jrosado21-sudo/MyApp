c5d3e6cc3bedd48e4283223c8e0ce3891274ffee
Jrosado21-sudo <61252905+Jrosado21-sudo@users.noreply.github.com>
2022-07-05 18:10:57 -0400

    Update AppTest.java

--- src/main/test/java/com/mycompany/app/AppTest.java
+++ src/main/test/java/com/mycompany/app/AppTest.java
@@ -1,20 +1,19 @@
-package com.mycompany.app;
+package fibonacci;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+class AppTest {
+
+	@Test
+	void testFibIteration() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFibRecursion() {
+		fail("Not yet implemented");
+	}
+
 }
